397fe362560842e91b5275ac8573d1cca127f2ad
Francois Robbertze <67432234+francoisrob@users.noreply.github.com>
2023-09-19 12:10:44 +0200

    Fixes

--- config.fish
+++ config.fish
@@ -1,4 +1,6 @@
-set PATH $HOME/.local/bin $HOME/bin $HOME/go/bin $PATH
+set DENO_INSTALL "/home/francois/.deno"
+set PATH $HOME/.local/bin $HOME/bin $HOME/go/bin $DENO_INSTALL/bin $PATH
+# export PATH="$DENO_INSTALL/bin:$PATH"
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
