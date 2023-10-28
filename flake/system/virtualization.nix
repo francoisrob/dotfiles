92723135ff369e08ec7ffd667b3116af3d65c6e4
Francois Robbertze <67432234+francoisrob@users.noreply.github.com>
2023-10-28 21:57:53 +0200

    feat: add virt-man

--- flake/system/virtualization.nix
+++ flake/system/virtualization.nix
@@ -1,13 +1,33 @@
-{...}: {
+{pkgs, ...}: {
   virtualisation = {
     docker.rootless = {
       enable = true;
       setSocketVariable = true;
     };
-    libvirtd.enable = true;
+    libvirtd = {
+      enable = true;
+      qemu = {
+        swtpm.enable = true;
+        ovmf.enable = true;
+        ovmf.packages = [ pkgs.OVMFFull.fd ];
+      };
+    };
+    spiceUSBRedirection.enable = true;
   };
   systemd.services."user@".serviceConfig = {
     Delegate = "cpu cpuset io memory pids";
   };
+  programs.dconf.enable = true;
   users.users.francois.extraGroups = ["libvirtd" "kvm" "docker"];
+
+  services.spice-vdagentd.enable = true;
+  environment.systemPackages = with pkgs; [
+    virt-manager
+    virt-viewer
+    spice spice-gtk
+    spice-protocol
+    win-virtio
+    win-spice
+    gnome.adwaita-icon-theme
+  ];
 }
