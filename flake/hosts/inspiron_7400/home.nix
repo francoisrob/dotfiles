848b7854a537e380e484b2f7c9bfcb21fbe1aeb7
Francois Robbertze <67432234+francoisrob@users.noreply.github.com>
2025-02-27 19:54:03 +0200

    flake

--- flake/hosts/inspiron_7400/home.nix
+++ flake/hosts/inspiron_7400/home.nix
@@ -1,8 +1,4 @@
-{
-  inputs,
-  pkgs,
-  ...
-}: {
+{pkgs, ...}: {
   home = {
     username = "francois";
     homeDirectory = "/home/francois";
@@ -154,48 +150,7 @@
       enable = true;
       lfs.enable = true;
     };
-    fish = {
-      enable = true;
-      shellInit = ''
-         set fish_greeting ""
-         set fish_home ~/.config/fish
 
-         set -gx PATH "$HOME/.local/share/bin/" $PATH
-         set -gx PATH "$HOME/.local/bin/" $PATH
-
-         # Volta
-         set -gx VOLTA_HOME "$HOME/.volta"
-         set -gx PATH "$VOLTA_HOME/bin" $PATH
-
-         # Rust
-         set -gx PATH "$HOME/.cargo/bin" $PATH
-
-         # Bun
-         set -gx PATH "$HOME/.bun/bin" $PATH
-
-         # set -gx EDITOR nvim
-         # set -gx VISUAL nvim
-
-         set -gx FZF_DEFAULT_COMMAND 'fd --type file'
-         set -gx FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
-
-         starship init fish | source
-         zoxide init fish | source
-
-         source "$HOME/.local/state/nix/profiles/home-manager/home-path/share/asdf-vm/asdf.fish"
-
-         function dev-api -d "npm run dev-api in current directory"
-             npm run dev-api
-        end
-
-         function fish_user_key_bindings
-             fish_default_key_bindings -M insert
-             fish_vi_key_bindings --no-erase insert
-         end
-
-         source ~/.secrets.fish
-      '';
-    };
     # Let Home Manager install and manage itself.
     home-manager.enable = true;
   };
