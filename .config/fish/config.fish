dd334879ce20d72ef96188922dcc2a9fc02053ff
Francois Robbertze <67432234+francoisrob@users.noreply.github.com>
2025-03-04 22:51:22 +0200

    fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,36 +1,26 @@
 set fish_greeting ""
 set fish_home ~/.config/fish
 
+set -gx PATH "$HOME/.local/share/bin/" $PATH
+set -gx PATH "$HOME/.local/bin/" $PATH
+
 # Volta
 set -gx VOLTA_HOME "$HOME/.volta"
 set -gx PATH "$VOLTA_HOME/bin" $PATH
 
-set -gx PATH "$HOME/.local/share/bin/" $PATH
-set -gx PATH "$HOME/.local/bin/" $PATH
-set -gx PATH /usr/local/go/bin $PATH
-
-# Go
-# set -gx PATH /usr/local/go/bin $PATH
-
 # Rust
 set -gx PATH "$HOME/.cargo/bin" $PATH
-set -gx PATH "$HOME/programs/platform-tools" $PATH
-set -gx PATH "$HOME/programs/mindustry-linux-64-bit" $PATH
 
 # Bun
 set -gx PATH "$HOME/.bun/bin" $PATH
-
-set -gx PROJECTS "$HOME/Projects"
-
-set -gx EDITOR nvim
-set -gx VISUAL nvim
 
 set -gx FZF_DEFAULT_COMMAND 'fd --type file'
 set -gx FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
 
 starship init fish | source
 zoxide init fish | source
-source ~/.asdf/asdf.fish
+
+source "$HOME/.local/state/nix/profiles/home-manager/home-path/share/asdf-vm/asdf.fish"
 
 function dev-api -d "npm run dev-api in current directory"
     npm run dev-api
@@ -40,6 +30,5 @@ function fish_user_key_bindings
     fish_default_key_bindings -M insert
     fish_vi_key_bindings --no-erase insert
 end
-
 
 source ~/.secrets.fish
