dc46d6425e1f75165ce34b68f49f9d02db7c47d1
Francois Robbertze <67432234+francoisrob@users.noreply.github.com>
2025-03-09 17:08:25 +0200

    fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,34 +1,38 @@
 set fish_greeting ""
 set fish_home ~/.config/fish
 
-set -gx PATH "$HOME/.local/share/bin/" $PATH
-set -gx PATH "$HOME/.local/bin/" $PATH
+
+# Paths (prepend to ensure priority)
+fish_add_path "$HOME/.local/share/bin"
+fish_add_path "$HOME/.local/bin"
+
+# Rust
+fish_add_path "$HOME/.cargo/bin"
+# Bun
+fish_add_path "$HOME/.bun/bin"
+
+fish_add_path "$HOME/.volta/bin"
 
 # Volta
 set -gx VOLTA_HOME "$HOME/.volta"
-set -gx PATH "$VOLTA_HOME/bin" $PATH
-
-# Rust
-set -gx PATH "$HOME/.cargo/bin" $PATH
-
-# Bun
-set -gx PATH "$HOME/.bun/bin" $PATH
 
 set -gx FZF_DEFAULT_COMMAND 'fd --type file'
 set -gx FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
 
-starship init fish | source
-zoxide init fish | source
-
+# ASDF
 source "$HOME/.local/state/nix/profiles/home-manager/home-path/share/asdf-vm/asdf.fish"
+
+source ~/.secrets.fish
 
 function dev-api -d "npm run dev-api in current directory"
     npm run dev-api
 end
 
-function fish_user_key_bindings
-    fish_default_key_bindings -M insert
-    fish_vi_key_bindings --no-erase insert
+function nd -d "run 'nix develop'"
+    nix develop --command fish
 end
 
-source ~/.secrets.fish
+# function fish_user_key_bindings
+#     fish_default_key_bindings -M insert
+#     fish_vi_key_bindings --no-erase insert
+# end
--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -1,4 +1,7 @@
 function fish_user_key_bindings
+    fish_default_key_bindings -M insert
+    fish_vi_key_bindings --no-erase insert
+
     starship init fish | source
     zoxide init fish | source
     fzf --fish | source
