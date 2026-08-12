6820a41a1e80cbc7111dafd5d04de6ec35d705f4
Francois Robbertze <67432234+francoisrob@users.noreply.github.com>
2026-08-12 14:13:28 +0200

    WIP

--- .config/hypr/gruvbox.lua
+++ .config/hypr/gruvbox.lua
@@ -9,89 +9,89 @@
 -- parity with the old $fooAlpha variables.
 
 return {
-    -- Backgrounds (dark0_hard .. dark4)
-    bg0_h       = "rgb(1d2021)",
-    bg0_h_alpha = "1d2021",
+	-- Backgrounds (dark0_hard .. dark4)
+	bg0_h = "rgb(1d2021)",
+	bg0_h_alpha = "1d2021",
 
-    bg0         = "rgb(282828)",
-    bg0_alpha   = "282828",
+	bg0 = "rgb(282828)",
+	bg0_alpha = "282828",
 
-    bg0_s       = "rgb(32302f)",
-    bg0_s_alpha = "32302f",
+	bg0_s = "rgb(32302f)",
+	bg0_s_alpha = "32302f",
 
-    bg1         = "rgb(3c3836)",
-    bg1_alpha   = "3c3836",
+	bg1 = "rgb(3c3836)",
+	bg1_alpha = "3c3836",
 
-    bg2         = "rgb(504945)",
-    bg2_alpha   = "504945",
+	bg2 = "rgb(504945)",
+	bg2_alpha = "504945",
 
-    bg3         = "rgb(665c54)",
-    bg3_alpha   = "665c54",
+	bg3 = "rgb(665c54)",
+	bg3_alpha = "665c54",
 
-    bg4         = "rgb(7c6f64)",
-    bg4_alpha   = "7c6f64",
+	bg4 = "rgb(7c6f64)",
+	bg4_alpha = "7c6f64",
 
-    -- Gray
-    gray        = "rgb(928374)",
-    gray_alpha  = "928374",
+	-- Gray
+	gray = "rgb(928374)",
+	gray_alpha = "928374",
 
-    -- Foregrounds (light0 .. light4)
-    fg0         = "rgb(fbf1c7)",
-    fg0_alpha   = "fbf1c7",
+	-- Foregrounds (light0 .. light4)
+	fg0 = "rgb(fbf1c7)",
+	fg0_alpha = "fbf1c7",
 
-    fg1         = "rgb(ebdbb2)",
-    fg1_alpha   = "ebdbb2",
+	fg1 = "rgb(ebdbb2)",
+	fg1_alpha = "ebdbb2",
 
-    fg2         = "rgb(d5c4a1)",
-    fg2_alpha   = "d5c4a1",
+	fg2 = "rgb(d5c4a1)",
+	fg2_alpha = "d5c4a1",
 
-    fg3         = "rgb(bdae93)",
-    fg3_alpha   = "bdae93",
+	fg3 = "rgb(bdae93)",
+	fg3_alpha = "bdae93",
 
-    fg4         = "rgb(a89984)",
-    fg4_alpha   = "a89984",
+	fg4 = "rgb(a89984)",
+	fg4_alpha = "a89984",
 
-    -- Bright accents
-    red         = "rgb(fb4934)",
-    red_alpha   = "fb4934",
+	-- Bright accents
+	red = "rgb(fb4934)",
+	red_alpha = "fb4934",
 
-    green       = "rgb(b8bb26)",
-    green_alpha = "b8bb26",
+	green = "rgb(b8bb26)",
+	green_alpha = "b8bb26",
 
-    yellow       = "rgb(fabd2f)",
-    yellow_alpha = "fabd2f",
+	yellow = "rgb(fabd2f)",
+	yellow_alpha = "fabd2f",
 
-    blue        = "rgb(83a598)",
-    blue_alpha  = "83a598",
+	blue = "rgb(83a598)",
+	blue_alpha = "83a598",
 
-    purple       = "rgb(d3869b)",
-    purple_alpha = "d3869b",
+	purple = "rgb(d3869b)",
+	purple_alpha = "d3869b",
 
-    aqua        = "rgb(8ec07c)",
-    aqua_alpha  = "8ec07c",
+	aqua = "rgb(8ec07c)",
+	aqua_alpha = "8ec07c",
 
-    orange       = "rgb(fe8019)",
-    orange_alpha = "fe8019",
+	orange = "rgb(fe8019)",
+	orange_alpha = "fe8019",
 
-    -- Neutral accents
-    neutral_red          = "rgb(cc241d)",
-    neutral_red_alpha    = "cc241d",
+	-- Neutral accents
+	neutral_red = "rgb(cc241d)",
+	neutral_red_alpha = "cc241d",
 
-    neutral_green        = "rgb(98971a)",
-    neutral_green_alpha  = "98971a",
+	neutral_green = "rgb(98971a)",
+	neutral_green_alpha = "98971a",
 
-    neutral_yellow       = "rgb(d79921)",
-    neutral_yellow_alpha = "d79921",
+	neutral_yellow = "rgb(d79921)",
+	neutral_yellow_alpha = "d79921",
 
-    neutral_blue         = "rgb(458588)",
-    neutral_blue_alpha   = "458588",
+	neutral_blue = "rgb(458588)",
+	neutral_blue_alpha = "458588",
 
-    neutral_purple       = "rgb(b16286)",
-    neutral_purple_alpha = "b16286",
+	neutral_purple = "rgb(b16286)",
+	neutral_purple_alpha = "b16286",
 
-    neutral_aqua         = "rgb(689d6a)",
-    neutral_aqua_alpha   = "689d6a",
+	neutral_aqua = "rgb(689d6a)",
+	neutral_aqua_alpha = "689d6a",
 
-    neutral_orange       = "rgb(d65d0e)",
-    neutral_orange_alpha = "d65d0e",
+	neutral_orange = "rgb(d65d0e)",
+	neutral_orange_alpha = "d65d0e",
 }
